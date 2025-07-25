70870d43eb3e30d6a28c270ba2c62c1ecca2e20a
Dan Aitmambetov <danat7075@gmail.com>
2025-07-26 04:02:34 +0500

    finalv1beforetests

--- swapi.py
+++ swapi.py
@@ -1,5 +1,5 @@
 import requests
-from bs4 import BeautifulSoup
+import os
 
 class APIRequester:
     def __init__(self, base_url='https://www.swapi.tech/api'):
@@ -23,13 +23,14 @@ class SWRequester(APIRequester):
 
 
     def get_sw_categories(self, endpoint=''):
+        category_dict = {}  
         try:
             response = requests.get(f'{self.base_url}/{endpoint}')
             response.raise_for_status()
             categories = response.json()
-            print("Доступные категории:")
-            for category in categories['result']:
-                print(category)
+            for category,url in categories['result'].items():
+                category_dict[category] = url
+            return category_dict.keys()
         except requests.ConnectionError as e:
             print(f'Произошла ошибка соединения с {self.base_url}')
             print(f'Ошибка соединения: {e}')
@@ -41,7 +42,7 @@ class SWRequester(APIRequester):
         try:
             response = requests.get(f'{self.base_url}/{sw_type}')
             response.raise_for_status()
-            return f'Результат категории {sw_type}: \n{response.text}'
+            return response.text
         except requests.ConnectionError as e:
             print(f'Произошла ошибка соединения с {self.base_url}')
             print(f'Ошибка соединения: {e}')
@@ -49,10 +50,26 @@ class SWRequester(APIRequester):
             print(f'Произошла ошибка запроса с {self.base_url}')
             print(f'Ошибка запроса: {e}')
     
+def save_sw_data(base_url='https://www.swapi.tech/api'):
+    directory = 'data'
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    requester = SWRequester(base_url)
+    requester.get_sw_categories()
+    for category in requester.get_sw_categories():
+        file_path = os.path.join(directory, f'{category}.txt')
+        with open(file_path, 'w') as file:
+            data = requester.get_sw_info(category)
+            file.write(data)
 
-
-api_requester = SWRequester()
+#api_requester = SWRequester()
 
 # Пример использования метода get_sw_info для получения информации о людях
-people_info = api_requester.get_sw_info("people")
-print(people_info)  # Печатаем весь ответ, который будет строкой
+"""people_info = api_requester.get_sw_categories()
+print(people_info)  # Печатаем весь ответ, который будет строкой"""
+"""response = requests.get('https://www.swapi.tech/api')
+response.raise_for_status()
+categories = response.json()
+print(categories)  # Печатаем весь ответ, который будет словарем"""
+
+save_sw_data()
